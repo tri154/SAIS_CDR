98b9010d553fb58a215d300a760b22ff3685d425
tri <triledacminh@gmail.com>
2025-06-03 15:19:22 +0700

    update

--- Code/info.py
+++ Code/info.py
@@ -41,8 +41,55 @@ class Info:
                             
         if self.DATASET == 'DocRED':
             self.load_DocRED()
-                    
-    
+        if self.DATASET == 'CDR':
+            self.load_CDR()
+
+    def load_CDR(self):
+        self.MODE_TRAIN = 'train_annotated'
+        self.MODE_DEV = 'dev'
+        self.MODE_TEST = 'test'
+
+        self.FILE_CORPUS_TRAIN = os.path.join(self.DIR_DATASET_ORI, f'{self.MODE_TRAIN}.json')
+        self.FILE_CORPUS_DEV = os.path.join(self.DIR_DATASET_ORI, f'{self.MODE_DEV}.json')
+        self.FILE_CORPUS_TEST = os.path.join(self.DIR_DATASET_ORI, f'{self.MODE_TEST}.json')
+        self.FILE_CORPUSES = {self.MODE_TRAIN: self.FILE_CORPUS_TRAIN, self.MODE_DEV: self.FILE_CORPUS_DEV,
+                              self.MODE_TEST: self.FILE_CORPUS_TEST}
+
+        self.FILE_INPUT_TRAIN = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_TRAIN}_inputs.pkl')
+        self.FILE_INPUT_DEV = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_DEV}_inputs.pkl')
+        self.FILE_INPUT_TEST = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_TEST}_inputs.pkl')
+        self.FILE_INPUTS = {self.MODE_TRAIN: self.FILE_INPUT_TRAIN, self.MODE_DEV: self.FILE_INPUT_DEV,
+                            self.MODE_TEST: self.FILE_INPUT_TEST}
+
+        self.FILE_RESULT_TRAIN = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_TRAIN}_results')
+        self.FILE_RESULT_DEV = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_DEV}_results')
+        self.FILE_RESULT_TEST = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_TEST}_results')
+        self.FILE_RESULTS = {self.MODE_TRAIN: self.FILE_RESULT_TRAIN, self.MODE_DEV: self.FILE_RESULT_DEV,
+                             self.MODE_TEST: self.FILE_RESULT_TEST}
+
+        self.FILE_TRUTH_TRAIN = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_TRAIN}_truth.pkl')
+        self.FILE_TRUTH_DEV = os.path.join(self.DIR_DATASET_PRO, f'{self.MODE_DEV}_truth.pkl')
+        self.FILE_TRUTHS = {self.MODE_TRAIN: self.FILE_TRUTH_TRAIN, self.MODE_DEV: self.FILE_TRUTH_DEV}
+
+        self.FILE_DEV_IN_TRAIN = os.path.join(self.DIR_DATASET_PRO, 'dev_in_train.pkl')
+        self.FILE_DEV_INS = {self.MODE_TRAIN: self.FILE_DEV_IN_TRAIN}
+
+        self.DATA_NER2ID = json.load(open(os.path.join(self.DIR_DATASET_ORI, 'ner2id.json'), 'r'))
+        self.DATA_REL2ID = json.load(open(os.path.join(self.DIR_DATASET_ORI, 'rel2id.json'), 'r'))
+        self.DATA_ID2NER = {v: k for k, v in self.DATA_NER2ID.items()}
+        self.DATA_ID2REL = {v: k for k, v in self.DATA_REL2ID.items()}
+
+        # self.DATA_NER2WORD = {'BLANK': 'blank', 'ORG': 'organization', 'LOC': 'location', 'TIME': 'time',
+        #                       'PER': 'person', 'MISC': 'miscellanea', 'NUM': 'number'}
+        self.DATA_NER2WORD = {'CHEM': 'chemical', 'DISE':'disease'}
+        self.DATA_REL2WORD = json.load(open(os.path.join(self.DIR_DATASET_ORI, 'rel_info.json'), 'r'))
+        self.DATA_NER2VEC = os.path.join(self.DIR_DATASET_PRO, 'ner2vec.npy')
+        self.DATA_REL2VEC = os.path.join(self.DIR_DATASET_PRO, 'rel2vec.npy')
+
+        self.ID_REL_THRE = self.DATA_REL2ID['Na']
+        self.NUM_NER = len(self.DATA_NER2ID)
+        self.NUM_REL = len(self.DATA_REL2ID)
+
     def load_DocRED(self):
         
         self.MODE_TRAIN = 'train_annotated'
