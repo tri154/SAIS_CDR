1fabac38a843976be6ed61689cc68cb39021d24e
tri <triledacminh@gmail.com>
2025-06-10 21:29:36 +0700

    update

--- src/main.py
+++ src/main.py
@@ -2,13 +2,19 @@ from config import Config
 from trainer import Trainer
 from model import Model
 from tester import Tester
+from preprocessing import Preprocessing
     
 if __name__ == '__main__':
     cfg = Config()
 
-    model = Model()
-    tester = Tester()
-    trainer = Trainer()
+    pre = Preprocessing(cfg)
+    train_set, dev_set, test_set = pre.train_set, pre.dev_set, pre.test_set
+
+    
+
+    # model = Model()
+    # tester = Tester()
+    # trainer = Trainer()
 
     #TODO:
     # do training stuffs here.
