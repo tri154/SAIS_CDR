806c19a6e24d0a2dd8f250a54c12adee977def63
tri154 <triledacminh@gmail.com>
2025-06-27 21:05:40 +0700

    Added Unet

--- src/model.py
+++ src/model.py
@@ -149,12 +149,12 @@ class Model(nn.Module):
             self.hidden_dim = 768 #NOTE: change if transformer changes.
         self.RGCN = RGCN(self.hidden_dim, self.hidden_dim, self.hidden_dim, 3, 3, self.cfg.type_dim).to(self.cfg.device)
         self.unet = AttentionUNet(3, 256, down_channel=256)
-        self.similarity = Similarity(self.hidden_dim)
+        self.similarity = Similarity(self.hidden_dim).to(self.cfg.device)
 
         self.loss = Loss(cfg)
-        self.W_h = nn.Linear(self.hidden_dim, self.hidden_dim)
-        self.W_t = nn.Linear(self.hidden_dim, self.hidden_dim)
-        self.RE_predictor_module = nn.Linear(self.hidden_dim * self.cfg.bilinear_block_size, self.cfg.num_rel)
+        self.W_h = nn.Linear(self.hidden_dim + 256, self.hidden_dim + 256)
+        self.W_t = nn.Linear(self.hidden_dim + 256, self.hidden_dim + 256)
+        self.RE_predictor_module = nn.Linear((self.hidden_dim + 256) * self.cfg.bilinear_block_size, self.cfg.num_rel)
 
         # doc_data = {'doc_tokens': doc_tokens, # list of token id of the doc. single dimension single dimension.
         #             'doc_title': doc_title,
@@ -271,48 +271,46 @@ class Model(nn.Module):
         u_outs = list()
         for u_input in u_inputs:
             temp = u_input.unsqueeze(0)
-            u_outs.append(self.unet(temp))
-            print(u_outs[-1].shape)
-# torch.Size([1, 6, 6, 256])
-# torch.Size([1, 9, 9, 256])
-# torch.Size([1, 6, 6, 256])
-# torch.Size([1, 5, 5, 256])
+            u_outs.append(self.unet(temp).squeeze(0))
+
+        batch_entity_embs = torch.cat(batch_entity_embs, dim=0)
+
+        head_entity_pairs = list()
+        tail_entity_pairs = list()
+        h_t = list()
+        batch_labels = list()
+
+        for did in range(len(start_entity_pos) - 1):
+            doc_epair_rels = batch_epair_rels[did]
+            offset = int(start_entity_pos[did])
+            for eid_h, eid_t in permutations(np.arange(offset, int(start_entity_pos[did + 1])), 2):
+                h_t.append(u_outs[did][eid_h - offset, eid_t - offset])
+                pair_labels = torch.zeros(self.cfg.num_rel)
+                for r in doc_epair_rels[(eid_h - offset, eid_t - offset)]:
+                    pair_labels[self.cfg.data_rel2id[r]] = 1
+                batch_labels.append(pair_labels)
+                head_entity_pairs.append(eid_h)
+                tail_entity_pairs.append(eid_t)
 
 
-        # _______
-        # head_entity_pairs = list()
-        # tail_entity_pairs = list()
-        # batch_labels = list()
+        h_t = torch.stack(h_t)
+        head_entity_pairs = torch.tensor(head_entity_pairs).to(self.cfg.device)
+        tail_entity_pairs = torch.tensor(tail_entity_pairs).to(self.cfg.device)
+        batch_labels = torch.stack(batch_labels).to(self.cfg.device)
 
-        # for did in range(len(start_entity_pos) - 1):
-        #     doc_epair_rels = batch_epair_rels[did]
-        #     offset = int(start_entity_pos[did])
-        #     for eid_h, eid_t in permutations(np.arange(offset, int(start_entity_pos[did + 1])), 2):
-        #         pair_labels = torch.zeros(self.cfg.num_rel)
-        #         for r in doc_epair_rels[(eid_h - offset, eid_t - offset)]:
-        #             pair_labels[self.cfg.data_rel2id[r]] = 1
-        #         batch_labels.append(pair_labels)
-        #         head_entity_pairs.append(eid_h)
-        #         tail_entity_pairs.append(eid_t)
+        head_entity_embs = torch.cat([batch_entity_embs[head_entity_pairs], h_t], dim=-1)
+        tail_entity_embs = torch.cat([batch_entity_embs[tail_entity_pairs], h_t], dim=-1)
 
 
-        # head_entity_pairs = torch.tensor(head_entity_pairs).to(self.cfg.device)
-        # tail_entity_pairs = torch.tensor(tail_entity_pairs).to(self.cfg.device)
-        # batch_labels = torch.stack(batch_labels).to(self.cfg.device)
+        head_entity_rep = torch.tanh(self.W_h(head_entity_embs))
+        tail_entity_rep = torch.tanh(self.W_t(tail_entity_embs))
 
-        # head_entity_embs = batch_entity_embs[head_entity_pairs]
-        # tail_entity_embs = batch_entity_embs[tail_entity_pairs]
+        head_entity_rep = head_entity_rep.view(-1, (self.hidden_dim + 256) // self.cfg.bilinear_block_size, self.cfg.bilinear_block_size)
+        tail_entity_rep = tail_entity_rep.view(-1, (self.hidden_dim + 256) // self.cfg.bilinear_block_size, self.cfg.bilinear_block_size)
+        batch_RE_reps = (head_entity_rep.unsqueeze(3) * tail_entity_rep.unsqueeze(2)).view(-1, (self.hidden_dim + 256) * self.cfg.bilinear_block_size)
+        batch_RE_reps = self.RE_predictor_module(batch_RE_reps)
 
-
-        # head_entity_rep = torch.tanh(self.W_h(head_entity_embs))
-        # tail_entity_rep = torch.tanh(self.W_t(tail_entity_embs))
-
-        # head_entity_rep = head_entity_rep.view(-1, self.hidden_dim // self.cfg.bilinear_block_size, self.cfg.bilinear_block_size)
-        # tail_entity_rep = tail_entity_rep.view(-1, self.hidden_dim // self.cfg.bilinear_block_size, self.cfg.bilinear_block_size)
-        # batch_RE_reps = (head_entity_rep.unsqueeze(3) * tail_entity_rep.unsqueeze(2)).view(-1, self.hidden_dim * self.cfg.bilinear_block_size)
-        # batch_RE_reps = self.RE_predictor_module(batch_RE_reps)
-
-        # if is_training:
-        #     return self.loss.ATLOP_loss(batch_RE_reps, batch_labels)
-        # else:
-        #     return self.loss.ATLOP_pred(batch_RE_reps), batch_labels
+        if is_training:
+            return self.loss.ATLOP_loss(batch_RE_reps, batch_labels)
+        else:
+            return self.loss.ATLOP_pred(batch_RE_reps), batch_labels
