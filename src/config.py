aa1701bfda02544e10008b0d7dfe8520cfaa1332
tri154 <triledacminh@gmail.com>
2025-07-02 14:25:46 +0700

    fixed multi-GPUs

--- src/config.py
+++ src/config.py
@@ -40,7 +40,7 @@ def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
-    dist.init_process_group("ncll", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def cleanup():
     dist.destroy_process_group()
